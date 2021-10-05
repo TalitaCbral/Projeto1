155d746d3deabd70001523365d60ca3d9df57af7
Elisama Talita Cabral Santana <82914088+TalitaCbral@users.noreply.github.com>
2021-10-05 20:22:10 -0300

    Projeto ok

---
+++ Backend/routes/filmes.routes.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const { route } = require('express/lib/application');
+const res = require('express/lib/response');
+const router = express.Router();
+
+//---------------------------------------------------------
+
+const filmes = [
+    {
+        id: Date.now(),
+        name: "Interstellar",
+        image: "https://images.theconversation.com/files/64127/original/2mdvqkw8-1415620078.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
+        genre: "Scy-fi",
+        rate: 10,
+        // status: false
+
+    }
+];
+
+//--------------------------------------------------------------
+
+//----Rota GET para mostrar todos os filmes pré-cadastrados-----
+router.get('/', (req, res)=>{
+    res.send(filmes)
+});
+
+//--------------------------------------------------------------
+
+//----Rota GET para mostrar os filmes por ID--------------------
+router.get('/:id', (req, res)=>{
+    const idParam = req.params.id;
+    const index = filmes.findIndex(filme => filme.id == idParam);
+    const filme = filmes[index];
+    res.send(filme);
+});
+
+//--------------------------------------------------------------
+
+//----Rota PUT para atualizar um item através do ID-------------
+router.put('/:id', (req, res)=>{
+    const filmeEdit = req.body;
+    const id = req.params.id;
+    let filmePreCadastrado = filmes.find((filme)=> filme.id == id);
+
+    filmePreCadastrado.name = filmeEdit.name;
+    filmePreCadastrado.image = filmeEdit.image;
+    filmePreCadastrado.genre = filmeEdit.genre;
+    filmePreCadastrado.rate = filmeEdit.rate;
+    // filmePreCadastrado.status = filmeEdit.status;
+
+    res.send({
+        message: `filme ${filmePreCadastrado.name} atualizado com sucesso`,
+        data: filmePreCadastrado
+    });
+});
+
+//--------------------------------------------------------------
+
+//----Rota de POST para adicionar um novo filme-----------------
+router.post('/add', (req, res)=>{
+    const filme = req.body;
+    filme.id = Date.now();
+    filmes.push(filme);
+    res.status(201).send({
+        message: 'Filme cadastrado com sucesso',
+        data: filme
+    });
+})
+
+//--------------------------------------------------------------
+
+//----Rota DELETE que apaga o filme pelo ID---------------------
+router.delete('/:id', (req, res)=>{
+    const id = req.params.id;
+    const index = filmes.findIndex((filme)=> filme.id == id);
+    filmes.splice(index, 1);
+
+    res.send({
+        message: `Filme excluido com sucesso!`})
+})
+
+
+//-------------Exportação do routes-----------------------------
+module.exports = router;
---
+++ Frontend/index.js
@@ -0,0 +1,135 @@
+const caminhoApi = 'http://localhost:3000/filmes';
+const lista = document.getElementById('lista');
+let edicao = false;
+let idEdicao = 0;
+
+//----RENDERIZAR OS FILMES CADASTRADOS--------
+
+const getFilmes = async () => {
+    const response = await fetch(caminhoApi);
+
+    const data = await response.json();
+    console.log(data);
+    
+    data.map((filme)=>{
+        lista.insertAdjacentHTML('beforeend', `
+            <div class="divCards">
+            
+                <h2 >Filme: <span class="movieName">${filme.name}</span></h2>
+                <img class="igmUrl"src="${filme.image}" alt="Imagem do Filme">
+                <p>Gênero: <span class="movieGenre">${filme.genre}</span></p>
+                <p>Nota: <span class="rate">${filme.rate}</span></p>
+                <button type="button" class="button" onclick="putFilme(${filme.id})">Editar</button>
+                <button type="button" class="button" onclick="deleteFilme(${filme.id})">Excluir</button>
+                </br>
+
+            </div>
+        `)
+    })
+
+}
+getFilmes();
+
+//------------ADICIONAR---------------
+
+const submitForm = async (evento) => {
+    evento.preventDefault();
+
+    let name = document.getElementById('name');
+    let image = document.getElementById('image');
+    let genre = document.getElementById('genre');
+    let rate = document.getElementById('rate');
+
+    const filme = {
+        name : name.value,
+        image : image.value,
+        genre : genre.value,
+        rate : rate.value
+    }
+    
+    if(!edicao) {
+        const request = new Request(`${caminhoApi}/add`, {
+            method: 'POST',
+            body: JSON.stringify(filme),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        })
+
+        const response = await fetch(request);
+        const result = await response.json();
+
+        if(result) {
+            edicao = false;
+            getFilmes();
+        }
+
+    } else {
+        const request = new Request(`${caminhoApi}/${idEdicao}`, {
+            method: 'PUT',
+            body: JSON.stringify(filme),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        })
+
+        const response = await fetch(request);
+        const result = await response.json();
+
+        if(result){
+            edicao = false;
+            getFilmes();
+        }
+    }
+
+
+    name.value = '';
+    image.value = '';
+    genre.value = '';
+    rate.value = '';
+
+    lista.innerHTML = '';
+
+}
+
+
+//----------ATUALIZAR----------------
+
+const getFilmeById = async (id) => {
+    const response = await fetch(`${caminhoApi}/${id}`);
+    return filme = response.json();
+}
+
+const putFilme = async (id) => {
+    edicao = true;
+    idEdicao = id;
+
+    const filme = await getFilmeById(id);
+
+    let nameElement = document.getElementById('name');
+    let imageElement = document.getElementById('image');
+    let genreElement = document.getElementById('genre');
+    let rateElement = document.getElementById('rate');
+
+    nameElement.value = filme.name;
+    imageElement.value = filme.image;
+    genreElement.value = filme.genre;
+    rateElement.value = filme.rate;
+
+    edicao = false;
+
+}
+
+//-----DELETAR---------------------------------------
+
+const deleteFilme = async (id) => {
+    const request = new Request(`${caminhoApi}/${id}`, {
+        method: 'DELETE',
+    })
+    const response = await fetch(request);
+    const data = await response.json();
+    console.log(data.message);
+
+    lista.innerHTML = '';
+    getFilmes();
+}
